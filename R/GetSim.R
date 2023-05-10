62b8284f10f47f5c19858a072d277192e7a94eb8
flanigangj <121189404+flanigangj@users.noreply.github.com>
2023-05-10 14:12:25 -0400

    Update GetSim.R

--- R/GetSim.R
+++ R/GetSim.R
@@ -1,12 +1,12 @@
 
 GetSim <- function(drugName=DrugName, DRS= drugResponseScore,GES= GeneEffectScores ){
-  ## make sure that users already use the common cell lines in both datasets >> the result will be precise.
-## if user feed only one drug and matrix is one ddrug.
+  ## Make sure that users already use the common cell lines in both datasets so the result will be precise.
+  ## If the user feeds only one drug into the matrix:
   common.c=intersect(colnames(DRS),
                      colnames(GES))
   DRS.c=DRS[drugName,common.c ]
   GES.c=GES[,common.c]
-  ## calculate corelation.
+  ## Calculate corelation:
   out.Sim.GES.DRS  =sapply(1:nrow(GES.c),function(x)
     unlist(cor.test_trimmed_v0.default(DRS.c, GES.c[x,])))
   out.Sim.GES.DRS =t(out.Sim.GES.DRS)
