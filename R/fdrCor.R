89bd586cb5ace35ff31916e7f2a4b2bd7cb2cae6
flanigangj <121189404+flanigangj@users.noreply.github.com>
2023-05-10 14:22:06 -0400

    Update fdrCor.R

--- R/fdrCor.R
+++ R/fdrCor.R
@@ -1,9 +1,9 @@
 #' fdrcorr
-## this function is used to calculate for FDR based on the list of P val.
+## this function is used to calculate the false discovery rate (FDR) based on the list of P values.
 
 fdrCor <- function(test_list) {
   # Use the p.adjust function from the stats package in R to adjust the p-values
-  # in the test_list using the false discovery rate method.
+  # Use the test_list and apply the false discovery rate method.
   p.adjust(test_list, method = 'fdr')
 }
 
