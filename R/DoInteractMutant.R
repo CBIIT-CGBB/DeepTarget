5ac1d09d57f34798f8997495ea89b89b0bf5cacf
flanigangj <121189404+flanigangj@users.noreply.github.com>
2023-05-10 14:10:07 -0400

    Update DoInteractMutant.R

--- R/DoInteractMutant.R
+++ R/DoInteractMutant.R
@@ -3,7 +3,7 @@
 
 DoInteractMutant <- function(Predtargets=Drug.targeted.sim,Mutant=Mutant,DRS=DRS, GES=GES )
   {
-  # getting only the overlapped samples. The best way to do is to get overlapped samples for all.
+  # Get only the overlapping samples. The best way to do this is to get overlapped samples for all.
     L.c <- list( KO= colnames(GES),
                 Drug_Prism= colnames(DRS),
                 Mutation= colnames(Mutant))
@@ -11,11 +11,11 @@ DoInteractMutant <- function(Predtargets=Drug.targeted.sim,Mutant=Mutant,DRS=DRS
 
   interactFeatures=lapply(1:nrow(Predtargets), function(x)
   {
-    ## drug response based on the drug ID
+    ## Drug response based on the drug ID
     DRS.f =errHandle(DRS[Predtargets[x,1],CL.M])
     ## Gene effect scores based on the targeted gene
     GES.f=errHandle(GES[Predtargets[x,3],CL.M])
-    ## groups based on the cut-off. Lower and higher based on the targed gene
+    ## Groups based on the cut-off. Lower and higher based on the targed gene.
     Mutant.Group =errHandle(Mutant[Predtargets[x,3],CL.M] )
     ## True is low, False is high.
     errHandle(summary(lm(DRS.f ~
